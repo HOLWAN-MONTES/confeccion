44969901b6cf6746cef92b9123530a71a6019b1e
aldana1311 <jdcarvajal562@misena.edu.co>
2021-07-14 11:09:12 -0500

    Arreglo formulario de insumos y maquinaria

--- js/users/admin/created_insu.js
+++ js/users/admin/created_insu.js
@@ -25,32 +25,69 @@ enviarInsu.addEventListener("click", (e) => {
         method:"POST",
         body:dato
     }).then(res => res.text()).then(info => {
-        if (info == 1) {
-            alert("Usuario creado existosamente")
+        if(info == 1){
+            Swal.fire({
+                title: 'Error!',
+                text: 'El insumo ya existe.',
+                icon: 'error',
+                confirmButtonText: 'Continuar'
+            })
             formInsu.reset()
-        }else if (info == 2){
-            alert('Lle correctamente el formulario');
+
+        }else if(info == 2){
+            Swal.fire({
+                title: 'Advertencia!',
+                text: 'Por favor rellena el formulario correctamente o verifica que no hallan datos vacios.',
+                icon: 'warning',
+                confirmButtonText: 'Continuar'
+            })
+
         }
-        else if (info == 3){
-            alert('El insumo ya ha sido creado')
-        } 
+        else{
+            Swal.fire({
+                title: 'Registrado!',
+                text: 'Se registro el insumo.',
+                icon: 'success',
+                confirmButtonText: 'Continuar'
+                
+            })
+            formInsu.reset()
+        }
     })
 })
+const ingre_insumo = document.getElementById("CrearInsumoForm")
+const inputs_insu = document.querySelectorAll("#CrearInsumoForm input")
 
-//^ buscador 
+const expresiones_insu = {
+    nombre_insu: /^[a-zA-Z0-9\s]{4,16}$/, // Letras y espacios, pueden llevar acentos.
+    metraje: /^\d{2,5}$/ // 2 a 5 numeros.  
+}
 
-datoBuscador.addEventListener("keyup", (e) => {
-    e.preventDefault()  
-    const dato = new FormData(formBuscador)
-
-    fetch("../../php/admin/buscador.php", {
-        method:"POST",
-        body:dato
-    }).then(res => res.text()).then(info => {
-        console.log(info)
-        conte_user.innerHTML=`${info}`
-    })
-})
+const formularioInsu = (e) =>{
+    switch (e.target.name){
+        case "nombre_tela":
+            if(expresiones_insu.nombre_insu.test(e.target.value)){
+                document.getElementById('grupo__nombre_insu').classList.remove('formulario__grupo-incorrecto_insu');
+                document.getElementById('grupo__nombre_insu').classList.add('formulario__grupo-correcto_insu');
+                document.querySelector('#grupo__nombre_insu i').classList.add('fa-check-circle');
+                document.querySelector('#grupo__nombre_insu i').classList.remove('fa-times-circle');
+                document.querySelector('#grupo__nombre_insu .formulario__input-error_insu').classList.remove('formulario__input-error-activo_insu');
+               
+            }else{
+                document.getElementById('grupo__nombre_insu').classList.add('formulario__grupo-incorrecto_insu');
+                document.getElementById('grupo__nombre_insu').classList.remove('formulario__grupo-correcto_insu');
+                document.querySelector('#grupo__nombre_insu i').classList.add('fa-times-circle');
+                document.querySelector('#grupo__nombre_insu i').classList.remove('fa-check-circle');
+                document.querySelector('#grupo__nombre_insu .formulario__input-error_insu').classList.add('formulario__input-error-activo_insu');
+               
+            }
+        break;
+    }
+}
+inputs_mate.forEach((input) => {
+    input.addEventListener('keyup', formularioInsu);
+    input.addEventListener('blur', formularioInsu);
+});
 
 //^ busacar por el tipo de usuario
 
@@ -96,4 +133,50 @@ todos.addEventListener("click", (e) => {
     
     e.preventDefault()
     actualizar()
+})
+
+
+
+//CONEXION AL ARCHIVO PHP CON EL FORMLARIO DE TIPO DE INSUMO
+const formTipInsu = document.getElementById('fomrTipinsu')
+const enviarTipInsu = document.getElementById('btnTipinsu')
+
+enviarTipInsu.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    const formMarcaM = new FormData(formTipInsu)
+
+    fetch("../../php/admin/tipo_insumo.php", {
+        method:"POST",
+        body:formMarcaM
+    })
+    .then(res => res.text()).then(info => {
+        if(info == 1){
+            Swal.fire({
+                title: 'Error!',
+                text: 'La marca del material textil ya existe.',
+                icon: 'error',
+                confirmButtonText: 'Continuar'
+            })
+            formTipInsu.reset()
+        }else if(info == 2){
+            Swal.fire({
+                title: 'Advertencia!',
+                text: 'Por favor rellena el formulario correctamente.',
+                icon: 'warning',
+                confirmButtonText: 'Continuar'
+            })
+            
+        }else{
+            Swal.fire({
+                title: 'Registrado!',
+                text: 'Se registro la marca del material textil.',
+                icon: 'success',
+                confirmButtonText: 'Continuar'
+                
+            })
+            formTipInsu.reset()
+        }
+    })
+
 })
