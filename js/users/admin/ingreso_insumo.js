d264e61306c270b8890ddb1b6f97d252c4bae53a
OscarLlanos1412 <70176989+OscarLlanos1412@users.noreply.github.com>
2021-07-17 02:16:30 -0500

    Termine backend de ingreso de usuarios falta estilos

--- js/users/admin/ingreso_insumo.js
+++ js/users/admin/ingreso_insumo.js
@@ -44,6 +44,7 @@ $(document).ready(function(){
     });
     $(document).on("click", ".edit_btn", editarInsumo);  
     $(document).on("click", "#envia_ing_ins", guardarInsumo);  
+    $(document).on("click", "#cancel", cancelarInsumo);  
 });
 
 function agregar(){
@@ -79,12 +80,21 @@ function agregar(){
         canti_edi = "canti" + can_edi;
         const deleteButton = "<button class='deleteButton'>Eliminar</button>";
         var editButton = `<button class='edit_btn ${id_datos}'>Editar</button>`;
-        fila='<tr id="'+ id_row +'" class="tr_val" name="fila_tb_din[]"><td name="row_cat[]">'+ categorias +'</td><td name="row_name[]">'+ name +'</td><td id="'+ canti_edi +'" class="cant_ed" name="row_canti[]">'+ cantidad +'</td><td>'+ editButton + deleteButton +'</td></tr>';
-
+        fila ='<tr id="'+ id_row +'" class="tr_val" name="fila_tb_din[]"><td class="td_cat" name="row_cat[]">'+ categorias +'</td><td class="td_nom" name="row_name[]">'+ name +'</td><td id="'+ canti_edi +'" class="cant_ed" name="row_canti[]">'+ cantidad +'</td><td class="td_botones">'+ editButton + deleteButton +'</td></tr>';
+        
         $('#tabla_ing_insu').append(fila);
         $('#categoria option:first').prop('selected', true); //Vacia los campos de los selects cuando agrega
         $('#nom_catego').find('option').not(':first').remove(); //Vacia los campos de los selects cuando agrega
         $('input[type="number"]').val(''); //Vacia el campo del input 
+        // const cattegori = document.getElementsByTagName("td");
+        // // console.log(cattegori);
+        // const nommb = document.getElementsByClassName("td_nom");
+        // // console.log(nommb);
+        // const cantti = document.getElementsByClassName("cant_ed");
+        // // console.log(cantti);
+        // const botones = document.getElementsByClassName("td_botones");
+        // cattegori.style.border = "1px solid #23ad9dc5";
+        // console.log(botones);
     }
     else{
         Swal.fire({
@@ -99,7 +109,6 @@ $(document).on("click", ".deleteButton", function(e){
     e.preventDefault();
     $(this).parents('tr').eq(0).remove();
     datos.splice(id_row, 1);
-    console.log(datos);
     Swal.fire({
         title: 'Eliminado!',
         text: 'El insumo se elimino',
@@ -111,66 +120,15 @@ function editarInsumo(e){
     e.preventDefault();
     var canti_edi = parseInt(prompt("Cantidad Nueva"));
     const id = e.target.classList[1] //^ me trae el identificador del botn
-    const id_lista =id-2 //^ le resto 2 para igualarlo a index de la lista
+    console.log(id);
+    const id_lista = id-2 //^ le resto 2 para igualarlo a index de la lista
+    console.log(id_lista);
     if(canti_edi){
         var _this = this;//Esta linea recupera el elemento o lugar el cual se llamo esta funcion
         var cantid = editarSelec(_this).innerHTML = canti_edi;//Llama a la funcion y recibe el objeto el cual es donde se llama la funcion
-        // console.log(cantid);
-        $(this).parent().parent().find("td:eq(2)").html(canti_edi);
-        var llaves = Object.keys(datos);
-        console.log(`linea 121 ${llaves}`);
-        // let i = 0;
-        for(i = 0; i < datos.length; i++) {
-            console.log(`linea 124 ${llaves}`);
-            console.log(`linea 125 ${llaves.length}`);
-        // console.log(i);
-            // console.log(datos);
-            // var nuevo = 
-            // console.log(nuevo);
-        
-        }
-        
-        
-        // var llaves = Object.keys(datos);
-        //     console.log(llaves);
-        // datos.forEach((item, index) => {
-        //     console.log(index);
-        //     console.log("a[" + index + "] =" + item);
-        //     datos[index]["cantidad"] = cantid;
-        //     if(index > 0){
-        //         console.log("Si sirvio el if");
-        //         datos[index]["cantidad"] = cantid;
-        //         console.log(datos)
-        //     }
-        //     console.log(datos);
-        //     // datos[0].cantidad = cantid;
-        // });
-        datos[id_lista]['cantidad'] = cantid;
-        console.log(datos);
-        
-        // console.log(datos);
-        // var llaves = Object.keys(datos);
-        // console.log(llaves);
-        // for(let i = 0; i < llaves.length; i++) {
-        //     // console.log(llaves);
-        //     // console.log(llaves.length);
-        //     // console.log(i);
-        //     console.log(datos);
-        //     datos[i].cantidad = cantid;
-            
-        // }
-        console.log(datos);
-        // datos[llaves]['cantidad'] = cantid;
-        if(datos[llaves]['id_datos'] == 1){
-            console.log("Parece que sirve el if");
-            datos[llaves]['cantidad'] = cantid;
-        }
-        else if(datos['id_datos'] > 1){
-            console.log("Parece que sirve el elif");
-            datos[llaves]['cantidad'] = cantid;
-        }
-        console.log(datos);
-        // datos[].cantidad = cantid;//De esta manera 
+        $(this).parent().parent().find("td:eq(2)").html(canti_edi);//Reemplaza el valor actual html por valor editado y lo muestra en el html
+
+        datos[id_lista]['cantidad'] = cantid; //Asigno el valor nuevo al arreglo
     }
     else{
         Swal.fire({
@@ -191,38 +149,56 @@ function editarSelec(objetoPresionado){
 
 function guardarInsumo(e){
     e.preventDefault();
-    console.log(datos);
+    // console.log(datos);
     var json = JSON.stringify(datos);//Convierte el array en una cadena de caracteres
-    $.ajax({
-        url: '../../php/admin/ingreso_insumos.php',
-        method: "POST",
-        data: "json="+json,
-        success: function(r){
-            console.log(r);
-            const envia = JSON.parse(r);
-            console.log(envia.status);
-            if(envia.status === 200){
-                datos = [];
-                console.log(datos);
-                Swal.fire({
-                    title: 'Agregado!',
-                    text: 'Se registro el ingreso del insumo',
-                    icon: 'success',
-                    confirmButtonText: 'Continuar'
-                });
-                document.getElementById('mostrar_insumos').innerHTML = '';
-                $('#proveedor option:first').prop('selected', true);
+    if(datos != ""){
+        $.ajax({
+            url: '../../php/admin/ingreso_insumos.php',
+            method: "POST",
+            data: "json="+json,
+            success: function(r){
+                const envia = JSON.parse(r);
+                if(envia.status === 200){
+                    datos = [];
+                    Swal.fire({
+                        title: 'Agregado!',
+                        text: 'Se registro el ingreso del insumo',
+                        icon: 'success',
+                        confirmButtonText: 'Continuar'
+                    });
+                    document.getElementById('mostrar_insumos').innerHTML = '';
+                    $('#proveedor option:first').prop('selected', true);
+                }
+                else {  
+                    // $('#estado').html('<hr><p>Error al guardar los datos.</p><hr>');
+                    Swal.fire({
+                        title: 'No agregado!',
+                        text: 'No se registro el insumo',
+                        icon: 'warning',
+                        confirmButtonText: 'Continuar'
+                    });
+                }
             }
-            else {  
-                // $('#estado').html('<hr><p>Error al guardar los datos.</p><hr>');
-                Swal.fire({
-                    title: 'No agregado!',
-                    text: 'No se registro el insumo',
-                    icon: 'warning',
-                    confirmButtonText: 'Continuar'
-                });
-            }
-        }
+        });
+    }
+    else {
+        Swal.fire({
+            title: 'Error!',
+            text: 'Eliga una opcion para el proveedor y/o ingrese valores',
+            icon: 'error',
+            confirmButtonText: 'Continuar'
+        });
+    }
+}
+function cancelarInsumo(e){
+    e.preventDefault();
+    datos = [];
+    Swal.fire({
+        title: 'Cancelado!',
+        text: 'Se cancelo el ingreso del insumo',
+        icon: 'info',
+        confirmButtonText: 'Continuar'
     });
-    console.log(json);
+    document.getElementById('mostrar_insumos').innerHTML = '';
+    $('#proveedor option:first').prop('selected', true);
 }
