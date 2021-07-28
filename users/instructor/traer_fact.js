92465fe5d0a270118275e9f3918bbed0dc648104
OscarLlanos1412 <70176989+OscarLlanos1412@users.noreply.github.com>
2021-07-28 03:53:38 -0500

    Arreglamos lo de validar devolucion

--- users/instructor/traer_fact.js
+++ users/instructor/traer_fact.js
@@ -7,3 +7,20 @@ document.addEventListener('click',(e) => {
         window.open(`devoluciones.php?trayendo_fact=${num_factura}`,'','width= 600,height=500, toolbar=NO');void(null);
     }
 })
+document.addEventListener('click', (e) =>{
+    e.preventDefault();
+    const id = document.getElementById('cantidad_devolver').value;
+    var json = document.getElementById('num_fac').innerText;
+    const can = document.getElementById('canti').innerText;
+    var form = $(this);
+    $.ajax({
+        type: 'GET',
+        url: `validar_devol.php?id=${id}&num=${json}&cant=${can}`,
+        data: form.serialize(),
+        success: function(data){
+            console.log(data);
+            // $('.contenedorCajaInventario').html('');
+            // $('.contenedorCajaInventario').append(data);
+        }
+    })
+})
